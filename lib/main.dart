132f359c98b58d81fa8ed487189620fe3dac5ded
Axiks <secure@mail.com>
2021-03-17 21:28:19 +0200

    fix match

--- lib/main.dart
+++ lib/main.dart
@@ -80,6 +80,8 @@ class _CalculatorState extends State<CalculatorView>{
                       Spacer(),
                       Text(
                         text,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
                         style: GoogleFonts.montserrat(
                           fontSize: 100,
                           fontWeight: FontWeight.w200,
@@ -281,6 +283,8 @@ class _CalculatorState extends State<CalculatorView>{
                         flex:4,
                         child:  Text(
                         text,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 3,
                         style: GoogleFonts.montserrat(
                           fontSize: 100,
                           fontWeight: FontWeight.w200,
@@ -295,129 +299,163 @@ class _CalculatorState extends State<CalculatorView>{
         )
     );
   }
-  //Calculator logic
-  dynamic text ='0';
-  double numOne = 0;
-  double numTwo = 0;
 
-  dynamic result = '';
-  dynamic finalResult = '';
-  dynamic opr = '';
-  dynamic preOpr = '';
-  void calculation(btnText) {
-    if(btnText  == 'AC') {
-      text ='0';
-      numOne = 0;
-      numTwo = 0;
-      result = '';
-      finalResult = '0';
-      opr = '';
-      preOpr = '';
+  String format(double n) {
+    String formatedNum = "";
+    if((n % 1) == 0){
+      formatedNum = n.toInt().toString();
+    }else{
+      formatedNum = n.toString();
+    }
+    //n.toStringAsFixed(n.truncateToDouble() == n ? 0 : 8);
+    return formatedNum;
+  }
 
-    } else if( opr == '=' && btnText == '=') {
-
-      if(preOpr == '+') {
-        finalResult = add();
-      } else if( preOpr == '-') {
-        finalResult = sub();
-      } else if( preOpr == 'x') {
-        finalResult = mul();
-      } else if( preOpr == '/') {
-        finalResult = div();
+  String render(double num1, double num2, String operation){
+    String result = "|";
+    if(num1 != null){
+      result =  format(num1);
+    }
+    if(operation.isNotEmpty){
+      result += " " + operation;
+      if(num2!=0){
+        result += " " +  format(num2);
       }
-
-    } else if(btnText == '+' || btnText == '-' || btnText == 'x' || btnText == '/' || btnText == '=') {
-
-      if(numOne == 0) {
-        numOne = double.parse(result);
-      } else {
-        numTwo = double.parse(result);
-      }
-
-      if(opr == '+') {
-        finalResult = add();
-      } else if( opr == '-') {
-        finalResult = sub();
-      } else if( opr == 'x') {
-        finalResult = mul();
-      } else if( opr == '/') {
-        finalResult = div();
-      }
-      preOpr = opr;
-      opr = btnText;
-      result = '';
-    }
-    else if(btnText == '%') {
-      result = numOne / 100;
-      finalResult = doesContainDecimal(result);
-    }
-    else if(btnText == '.') {
-      if(!result.toString().contains('.')) {
-        result = result.toString()+'.';
-      }
-      finalResult = result;
-    }
-    else if(btnText == '+/-') {
-      result.toString().startsWith('-') ? result = result.toString().substring(1): result = '-'+result.toString();
-      finalResult = result;
-    }
-    else {
-      result = result + btnText;
-      finalResult = result;
-    }
-
-    setState(() {
-      text = finalResult;
-    });
-
-  }
-
-  String add() {
-    result = (numOne + numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String sub() {
-    result = (numOne - numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-  String mul() {
-    result = (numOne * numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-  String div() {
-    result = (numOne / numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String doesContainDecimal(dynamic result) {
-    if(result.toString().contains('.')) {
-      List<String> splitDecimal = result.toString().split('.');
-      if(!(int.parse(splitDecimal[1]) > 0))
-        return result = splitDecimal[0].toString();
     }
     return result;
   }
 
-}
+  //Calculator logic
+  dynamic text ='0';
+  double numOne = null;
+  double numTwo = null;
 
-class HomePage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: OrientationBuilder(
-        builder: (builder, orientation){
-          if (orientation == Orientation.portrait){
-            return CalculatorView();
-          } else{
-            return Text("Lanshagt");
+  dynamic opr = '';
+  dynamic commaOne = false;
+  dynamic commaTwo = false;
+  void calculation(String btnText) {
+    switch(btnText) {
+      case 'AC': {
+        numOne = null;
+        numTwo = null;
+        opr = '';
+        commaOne = false;
+        commaTwo = false;
+      }
+      break;
+
+      case '+': {
+        opr = btnText;
+        commaOne = false;
+        commaTwo = false;
+      }
+      break;
+
+      case '-': {
+        opr = btnText;
+        commaOne = false;
+        commaTwo = false;
+      }
+      break;
+
+      case '*': {
+        opr = btnText;
+        commaOne = false;
+        commaTwo = false;
+      }
+      break;
+
+      case '/': {
+        opr = btnText;
+        commaOne = false;
+        commaTwo = false;
+      }
+      break;
+
+      case '=': {
+        //statements;
+        switch(opr) {
+          case '+': {
+            numOne+=numTwo;
           }
-          },
-      )
-    );
-  }
+          break;
+
+          case '-': {
+            numOne-=numTwo;
+          }
+          break;
+
+          case '*': {
+            numOne*=numTwo;
+          }
+          break;
+
+          case '/': {
+            numOne/=numTwo;
+          }
+          break;
+        };
+        numTwo = 0;
+        opr = '';
+        commaOne = false;
+        commaTwo = false;
+      }
+      break;
+
+      case '.': {
+        commaOne = true;
+        commaTwo = true;
+      }
+      break;
+
+      default: {
+        //nums
+        if  (opr=='') {
+          String numOneString = "";
+          if(numOne==null){
+            numOneString = btnText;
+          }
+          else{
+            if(commaOne){
+              if((numOne % 1) == 0){
+                numOneString = format(numOne) + '.';
+              }else{
+                numOneString = format(numOne);
+              }
+            }else{
+              numOneString = format(numOne);
+            }
+            numOneString += btnText;
+          }
+          numOne = double.parse(numOneString);
+        }
+        else{
+          String numTwoString = "";
+          if(numTwo==null){
+            numTwoString = btnText;
+          }
+          else{
+            if(commaTwo){
+              if((numTwo % 1) == 0){
+                numTwoString = format(numTwo) + '.';
+              }else{
+                numTwoString = format(numTwo);
+              }
+            }else{
+              numTwoString = format(numTwo);
+            }
+            numTwoString += btnText;
+          }
+          numTwo = double.parse(numTwoString);
+        }
+      }
+      break;
+    }
+
+    setState(() {
+      text = render(numOne,numTwo,opr);
+    });
+
+ }
+
 }
